66516cb2c5cdfc0d8f24f2b58e3bccc90e83ac16
nothing <something>
2018-09-19 10:12:07 -0600

    added git ignore and some java crap

---
+++ java crap/e1_5_equationthingie.java
@@ -0,0 +1,23 @@
+/*
+by me
+date somethin in 2018
+
+prints equation
+*/
+
+class e1_5_equationthingie {
+	public static void main(String[] args) {
+		
+		System.out.println(       
+		
+		(
+		(9.5*4.5)-(2.5*3)
+		)
+		
+		/ (45.5 - 3.5)
+		
+		
+		);
+		
+	}
+}
